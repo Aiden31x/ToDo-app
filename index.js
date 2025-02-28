90438920cb3a5920449b7509dbe2474f1bddbe49
Aiden31x <aryandadwal2004@gmail.com>
2025-02-28 10:25:26 +0530

    Updated routes logic

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //basic boilerplate
 //with express.json() middleware
 
+const { todo } = require("./db");
 const { createTodo, updateTodo } = require("./types");
 
 
@@ -9,7 +10,7 @@ const app = express();
 
 app.use(express.json()); //allows to parse a json body
 
-app.post("/todo", function (req, res) {
+app.post("/todo", function async(req, res) {
 
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
@@ -18,23 +19,45 @@ app.post("/todo", function (req, res) {
             msg: "You sent the wrong inputs",
         })
         return;
-
     }
 
+    await todo.create({
+        title: createPayload.title,
+        description: createPayload.description,
+        completed: false
+    })
+    res.json({
+        msg: "todo created"
+    })
+
 
 })
 
-app.get("/todos", function (req, res) {
+app.get("/todos", async function (req, res) {
+    const todos = await todo.find({});
+    res.json({
+        todos
+    })
 
 })
 
-app.put("/completed", function (req, res) {
+app.put("/completed", async function (req, res) {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if (!parsedPayload.success) {
         res.status(411).json({
             msg: "You sent the wrong inputs.",
 
+        })
+
+        await todo.update({
+            _id: req.body.id
+        }, {
+            completed: true
+        })
+
+        res.json({
+            msg: "Todo mark as completed"
         })
     }
 
